ac8c537c42fdb8f7475f44ec53369c2b79003ecb
Daisuke Sakurai <d.sakurai0@gmail.com>
2025-09-04 23:16:35 +0900

    Initial commit

---
+++ install.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+set -e
+
+# Get the directory of this script
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+cd "$SCRIPT_DIR"
+
+sudo apt update
+sudo apt install -y ansible git
+
+# Run the Ansible playbook
+ansible-playbook -i inventory.ini playbook.yml --ask-become-pass
